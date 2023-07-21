015ffb76b76ed05a98017d4d79c7dee93b81998c
vinod-trial <vinodkumar.kutti@gmail.com>
2023-07-21 10:53:40 +0530

    learning maven

---
+++ src/main/java/clinic/programming/training/application.java
@@ -0,0 +1,10 @@
+package clinic.programming.training;
+public class application{
+    public application(){
+        System.out.println("Inside application");
+    }
+    public static void main(String args[]){
+        System.out.println("Starting application");
+        application app= new application();
+    }
+}
