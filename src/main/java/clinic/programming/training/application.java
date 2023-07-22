fdaf840860f1c0bad9f23ab5e1f1a1cb7a48aedc
vinod-trial <vinodkumar.kutti@gmail.com>
2023-07-22 15:58:43 +0530

    modified code test

--- src/main/java/clinic/programming/training/application.java
+++ src/main/java/clinic/programming/training/application.java
@@ -1,10 +1,29 @@
 package clinic.programming.training;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 public class application{
+
+    public int countWords(String words){
+        String[] seperateWords = StringUtils.split(words, ' ');
+        return (seperateWords == null)? 0 : seperateWords.length;
+    }
+    public void greet(){
+        List <String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+        for(String greeting : greetings){
+            System.out.println("Greeting: "+ greeting);
+        }
+    }
     public application(){
         System.out.println("Inside application");
     }
     public static void main(String args[]){
         System.out.println("Starting application");
-        application app= new application();
+        application app = new application();
+        app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println("Word count: "+count);
+        
     }
 }
